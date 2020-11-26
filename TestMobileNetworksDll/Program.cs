2838c3be58c5dba1aba636f602c0f3147cb35292
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-26 09:20:17 +0200

    Created new console project to test mobilenumbers dll.

---
+++ TestMobileNetworksDll/Program.cs
@@ -0,0 +1,16 @@
+﻿using BotswanaMobileNetworkOperators;
+using System;
+
+namespace TestMobileNetworksDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            var number = Console.ReadLine();
+            var output = BotswanaNetworkProvider.GetMobileNetworkProvider(number);
+            Console.WriteLine($"{output.Phone} - {output.NetworkProvider}");
+        }
+    }
+}
